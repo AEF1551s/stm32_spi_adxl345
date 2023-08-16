014ca447065c0fc80ab5d77b1bbe84d927aae9e1
Krisjanis Ivbulis <ivbulisk@gmail.com>
2023-08-16 19:23:48 +0300

    move inits to spi1.h. Add extern to pins

--- Src/main.cpp
+++ Src/main.cpp
@@ -18,34 +18,20 @@
 
 // Pin declaration
 /******SPI1 pins******/
-pin_struct_TypeDef nss_pin;
-pin_struct_TypeDef sck_pin;
-pin_struct_TypeDef miso_pin;
-pin_struct_TypeDef mosi_pin;
+extern pin_struct_TypeDef nss_pin;
+extern pin_struct_TypeDef sck_pin;
+extern pin_struct_TypeDef miso_pin;
+extern pin_struct_TypeDef mosi_pin;
 /********************/
+
 // Functions
 void clock_init()
 {
-  // GPIOA clock enable for PA4 - PA7 SPI pins
-  SET_BIT(RCC->AHB1ENR, RCC_AHB1ENR_GPIOAEN);
-
-  // SPI1 clock enable
-  SET_BIT(RCC->APB2ENR, RCC_APB2ENR_SPI1EN);
+  spi1_clock_init();
 }
 void pin_init()
 {
-  /******SPI1 pins******/
-  nss_pin = pin_setup(GPIOA, PIN4, ALTERNATE);
-  sck_pin = pin_setup(GPIOA, PIN5, ALTERNATE);
-  miso_pin = pin_setup(GPIOA, PIN6, ALTERNATE);
-  mosi_pin = pin_setup(GPIOA, PIN7, ALTERNATE);
-
-  // Set AF mode
-  SET_BIT(GPIOA->AFR[0], GPIO_AFRL_AFRL4_0 | GPIO_AFRL_AFRL4_3);
-  SET_BIT(GPIOA->AFR[0], GPIO_AFRL_AFRL5_0 | GPIO_AFRL_AFRL5_3);
-  SET_BIT(GPIOA->AFR[0], GPIO_AFRL_AFRL6_0 | GPIO_AFRL_AFRL6_3);
-  SET_BIT(GPIOA->AFR[0], GPIO_AFRL_AFRL7_0 | GPIO_AFRL_AFRL7_3);
-  /*********************/
+  spi1_pin_init();
 }
 
 int main(void)
